5e66ced29e732193f6ce915f329e1f6454eb53f4
argentlupine <argentlupine@icloud.com>
2026-05-08 18:01:27 +0200

    06.1 created parentnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,10 +21,7 @@ class HTMLNode():
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
-        self.tag = tag
-        self.value = value
-        self.props = props
+        super().__init__(tag, value, None, props)
     
     def to_html(self):
         if self.value is None:
@@ -34,3 +31,21 @@ class LeafNode(HTMLNode):
         else:
             props_output = self.props_to_html()
             return f"<{self.tag}{props_output}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None):
+        super().__init__(tag, None, children)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError
+        if self.children is None:
+            raise ValueError
+        else:
+            node_collector = ""
+            for child in self.children:
+                node_collector += child.to_html()
+            return f"<{self.tag}>{node_collector}</{self.tag}>"
