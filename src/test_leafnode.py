3683a8173b8b4d982abe7c91ffcadbf66e0f2e99
argentlupine <argentlupine@icloud.com>
2026-05-08 17:48:37 +0200

    06 finished adding leafnode

---
+++ src/test_leafnode.py
@@ -0,0 +1,16 @@
+import unittest
+from htmlnode import HTMLNode, LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    def test_leaf_unequal_to_html(self):
+        node = LeafNode(tag="p", value="Hello, world!")
+        self.assertNotEqual(node.to_html(), "<p>Goodbye world!</p>")
+    def test_leaf_equal_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com/"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com/">Click me!</a>')
+    def test_leaf_unequal_props(self):
+        node = LeafNode("a", "Press me?", {"href": "https://www.google.com/"})
+        self.assertNotEqual(node.to_html(), '<a href="https://www.google.com/">Click me!</a>')
