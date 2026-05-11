a9a380243b018fc50f15280210c970fb6314445d
argentlupine <argentlupine@icloud.com>
2026-05-11 20:31:39 +0200

    07 added parentnode and total tests

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -15,7 +15,7 @@ class TestParentNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
-    
+
     def test_to_html_with_two_children(self):
         child_node1 = LeafNode("a", "child")
         child_node2 = LeafNode("b", "child2")
@@ -23,4 +23,30 @@ class TestParentNode(unittest.TestCase):
         self.assertEqual(
             parent_node.to_html(),
             "<div><a>child</a><b>child2</b></div>",
+        )
+    
+    def test_to_html_with_children_props(self):
+        child_node = LeafNode("span", "child", {"href": "https://www.google.com/"})
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span href=\"https://www.google.com/\">child</span></div>"
+        )
+    
+    def test_to_html_no_tag(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode(None, [child_node])
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+
+    def test_to_html_no_children(self):
+        parent_node = ParentNode("div", None)
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+
+    def test_to_html_empty_children(self):
+        parent_node = ParentNode("div", [])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div></div>"
         )
