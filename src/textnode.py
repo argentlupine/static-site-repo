3c8bfcad2c62edffd5f990f4e0878e3a60b5516b
argentlupine <argentlupine@icloud.com>
2026-05-12 20:30:00 +0200

    08 added textnode to htmlnode function and associated tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
-    PLAIN = "text"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -24,3 +25,18 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type is TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type is TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type is TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type is TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type is TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type is TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise Exception(f"text_type {text_node.text_type} not a valid type")
