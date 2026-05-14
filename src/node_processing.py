c785c4d08cfb06a5b44229ae38803ea1ff2a09be
argentlupine <argentlupine@icloud.com>
2026-05-14 21:42:20 +0200

    08.2 made a few changes to function

--- src/node_processing.py
+++ src/node_processing.py
@@ -10,4 +10,6 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             raise Exception("Error: passed node is not a TextNode")
         if node.text_type is not TextType.TEXT:
             new_nodes_list.append(node)
-        elif 
+        else:
+            split_node = node.str.split(delimiter)
+            new_nodes_list.extend(split_node)
