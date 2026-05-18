a5137fd0f0646d7f901c0ee91138a21731230e5a
argentlupine <argentlupine@icloud.com>
2026-05-18 20:53:12 +0200

    08.5 established working function

--- src/node_processing.py
+++ src/node_processing.py
@@ -6,20 +6,22 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         raise Exception("No nodes passed to old_nodes")
     new_nodes_list = []
     for node in old_nodes:
-        if node is not TextNode:
+        if not isinstance(node, TextNode):
             raise Exception("Error: passed node is not a TextNode")
-        if node.text_type is not TextType.TEXT:
+        elif node.text_type is not TextType.TEXT:
             new_nodes_list.append(node)
-        if node.value.count(delimiter) % 2 != 0:
+        elif node.text.count(delimiter) % 2 != 0:
             raise Exception(f"Odd number of delimiter: {delimiter}")
         else:
-            split_nodes = node.str.split(delimiter)
+            split_nodes = node.text.split(delimiter)
             new_nodes_list.extend(split_nodes)
     count = 1
+    final_nodes_list = []
     for node in new_nodes_list:
         if count % 2 == 0:
-            node = TextNode(node, text_type=text_type)
+            final_nodes_list.append(TextNode(node, text_type=text_type))
         else:
-            node = TextNode(node, text_type=TextType.TEXT)
+            final_nodes_list.append(TextNode(node, text_type=TextType.TEXT))
         count += 1
-    return new_nodes_list
+    print(final_nodes_list)
+    return final_nodes_list
