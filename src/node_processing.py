901b3fea1bf167ed23f158cd5856d0c8444e2c8a
argentlupine <argentlupine@icloud.com>
2026-08-23 11:51:29 +0200

    12.1 created markdown to blocks function and associated text

--- src/node_processing.py
+++ src/node_processing.py
@@ -122,3 +122,17 @@ def text_to_textnodes(text):
     image_list = split_nodes_image(code_list)
     split_list = split_nodes_link(image_list)
     return split_list
+
+def markdown_to_blocks(markdown):
+    split_by_newline = markdown.split('\n\n')
+    # print(f'1. markdown is split into {split_by_newline}')
+    stripped_newlines = []
+    for newline in split_by_newline:
+        # print(f'2. iterating through "{newline}"')
+        stripped_newline = newline.strip().strip("\n")
+        # print(f'3. newline is stripped, now looks like: "{stripped_newline}"')
+        if stripped_newline == '':
+            continue
+        stripped_newlines.append(stripped_newline)
+    # print(f'4. finished iterating through list, final returned list = {stripped_newlines}')
+    return stripped_newlines
--- src/test_node_processing.py
+++ src/test_node_processing.py
@@ -1,6 +1,8 @@
 import unittest
 from textnode import TextNode, TextType
-from node_processing import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
+from node_processing import split_nodes_delimiter, extract_markdown_images, \
+    extract_markdown_links, split_nodes_image, split_nodes_link, \
+    text_to_textnodes, markdown_to_blocks
 
 class TestNodeProcessing(unittest.TestCase):
     def test_no_nodes_in_list(self):
@@ -641,3 +643,70 @@ class TestNodeProcessing(unittest.TestCase):
             ],
             nodes_created
         )
+    
+    # Now I write tests for splitting markdown blocks!
+
+#     def test_split_md_block(self):
+#         md = """
+# This is **bolded** paragraph
+
+# This is another paragraph with _italic_ text and `code` here
+# This is the same paragraph on a new line
+
+# - This is a list
+# - with items
+# """
+#         blocks = markdown_to_blocks(md)
+#         self.assertEqual(
+#             blocks,
+#             [
+#                 "This is **bolded** paragraph",
+#                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+#                 "- This is a list\n- with items",
+#             ]
+#         )
+    
+#     def test_split_md_block_just_newlines(self):
+#         md = """
+
+
+# """
+#         blocks = markdown_to_blocks(md)
+#         self.assertEqual(
+#             blocks,
+#             []
+#         )
+
+    def test_split_md_block_whitespace_check(self):
+        md = """
+    This is **bolded** paragraph      
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph"
+            ]
+        )
+
+    def test_split_md_block_multiple_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+This is _italiced_paragraph
+
+
+
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is _italiced_paragraph"
+            ]
+        )
+
+    
