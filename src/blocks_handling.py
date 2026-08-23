5f137542d0c7aac335f20e4cae15646c67c0e9ec
argentlupine <argentlupine@icloud.com>
2026-08-23 13:25:42 +0200

    13.1 created blocktype class, created function that confirms the attributes of a particular block, written first tests confirming the right attribute is applied

--- src/blocks_handling.py
+++ src/blocks_handling.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import re
+
 def markdown_to_blocks(markdown):
     split_by_newline = markdown.split('\n\n')
     # print(f'1. markdown is split into {split_by_newline}')
@@ -11,3 +14,52 @@ def markdown_to_blocks(markdown):
         stripped_newlines.append(stripped_newline)
     # print(f'4. finished iterating through list, final returned list = {stripped_newlines}')
     return stripped_newlines
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block):
+    # Identify header
+    header_split = block.split(maxsplit=1)
+    if re.fullmatch(r'#+', header_split[0]):
+        return BlockType.HEADING
+    
+    newlines_split = block.splitlines(keepends=True)
+    # print(f'newlines split looks like: {newlines_split}')
+
+    # print(f'code split looks like: {code_split}')
+    if newlines_split[0] == "```\n" and newlines_split[-1] == "```":
+        return BlockType.CODE
+
+    # Identify quotes, unordered and ordered lists
+    quote_bool = True
+    unord_list_bool = True
+    ord_list_nums = []
+    for line in newlines_split:
+        if line[0] != '>':
+            quote_bool = False
+        if line[0:2] != '- ':
+            unord_list_bool = False
+        first_space_split = line.split(maxsplit=1)
+        if re.fullmatch(r'[0-9]+\.', first_space_split[0]):
+            ord_list_nums.extend(re.findall(r'([0-9]+)', first_space_split[0]))
+            # print(f'appended {first_space_split[0]} to ord_list_nums')
+    if quote_bool:
+        return BlockType.QUOTE
+    if unord_list_bool:
+        return BlockType.UNORDERED_LIST
+    # print(f'final ordered list check is {ord_list_nums}')
+    # print(f'the first item is {ord_list_nums[0]}')
+    order_copy = ord_list_nums.copy()
+    order_copy.sort()
+    # print(f'if the ordered list is sorted: {order_copy}')
+    if ord_list_nums[0] == '1' and \
+        ord_list_nums == order_copy:
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_blocks_handling.py
+++ src/test_blocks_handling.py
@@ -1,5 +1,6 @@
 import unittest
-from blocks_handling import markdown_to_blocks
+from blocks_handling import BlockType
+from blocks_handling import markdown_to_blocks, block_to_block_type
 
 # Now I write tests for splitting markdown blocks!
 
@@ -67,4 +68,38 @@ This is _italiced_paragraph
             ]
         )
 
+    # Now testing the blocktype enum that's been created
+    # def test_block_to_blocktype_heading(self):
+    #     block = '### this is a heading'
+    #     self.assertEqual(
+    #         block_to_block_type(block),
+    #         BlockType.HEADING
+    #     )
     
+    # def test_block_to_blocktype_code(self):
+    #     block = '```\nthis is a code block\n```'
+    #     self.assertEqual(
+    #         block_to_block_type(block),
+    #         BlockType.CODE
+    #     )
+    
+    # def test_block_to_blocktype_quote(self):
+    #     block = '>quote line one\n> quote line two\n>  quote line three'
+    #     self.assertEqual(
+    #         block_to_block_type(block),
+    #         BlockType.QUOTE
+    #     )
+    
+    # def test_block_to_blocktype_unordered_list(self):
+    #     block = '- list item\n- another list item\n- a final list item'
+    #     self.assertEqual(
+    #         block_to_block_type(block),
+    #         BlockType.UNORDERED_LIST
+    #     )
+    
+    def test_block_to_blocktype_ordered_list(self):
+        block = '1. list item\n2. another list item\n3. a final list item'
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.ORDERED_LIST
+        )
