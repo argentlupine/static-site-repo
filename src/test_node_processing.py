7ccbb67f68d79914052d84abbc4291117dcf49ca
argentlupine <argentlupine@icloud.com>
2026-08-23 12:00:56 +0200

    12.3 created new blocks handling python file

--- src/test_node_processing.py
+++ src/test_node_processing.py
@@ -647,36 +647,36 @@ class TestNodeProcessing(unittest.TestCase):
     
     # Now I write tests for splitting markdown blocks!
 
-#     def test_split_md_block(self):
-#         md = """
-# This is **bolded** paragraph
+    def test_split_md_block(self):
+        md = """
+This is **bolded** paragraph
 
-# This is another paragraph with _italic_ text and `code` here
-# This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-# - This is a list
-# - with items
-# """
-#         blocks = markdown_to_blocks(md)
-#         self.assertEqual(
-#             blocks,
-#             [
-#                 "This is **bolded** paragraph",
-#                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-#                 "- This is a list\n- with items",
-#             ]
-#         )
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ]
+        )
     
-#     def test_split_md_block_just_newlines(self):
-#         md = """
+    def test_split_md_block_just_newlines(self):
+        md = """
 
 
-# """
-#         blocks = markdown_to_blocks(md)
-#         self.assertEqual(
-#             blocks,
-#             []
-#         )
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            []
+        )
 
     def test_split_md_block_whitespace_check(self):
         md = """
