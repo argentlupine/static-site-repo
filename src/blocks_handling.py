9f08e5ae643662dbc6caa7af2e47815bad402be0
argentlupine <argentlupine@icloud.com>
2026-08-24 19:11:19 +0200

    13.4 cleaned up even more with some beautiful list comprehensions

--- src/blocks_handling.py
+++ src/blocks_handling.py
@@ -24,29 +24,38 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered_list"
 
 def block_to_block_type(block):
-    header_split = block.split(maxsplit=1)
     if re.match(r'^#{1,6} ', block):
         return BlockType.HEADING
     
     if block.startswith("```\n") and block.endswith("```"):
         return BlockType.CODE
 
-    newlines_split = block.splitlines()
-    quote_bool = True
-    unord_list_bool = True
-    ord_list_bool = True
-    for i, line in enumerate(newlines_split):
-        if not line.startswith('>'):
-            quote_bool = False
-        if not line.startswith('- '):
-            unord_list_bool = False
-        if not line.startswith(f'{i+1}. '):
-            ord_list_bool = False
-    if quote_bool:
+    lines = block.splitlines()
+
+    if all(line.startswith('>') for line in lines):
         return BlockType.QUOTE
-    if unord_list_bool:
+
+    if all(line.startswith('- ') for line in lines):
         return BlockType.UNORDERED_LIST
-    if ord_list_bool:
+    
+    if all(line.startswith(f'{i+1}. ') for i, line in enumerate(lines)):
         return BlockType.ORDERED_LIST
+
+    # quote_bool = True
+    # unord_list_bool = True
+    # ord_list_bool = True
+    # for i, line in enumerate(newlines_split):
+    #     if not line.startswith('>'):
+    #         quote_bool = False
+    #     if not line.startswith('- '):
+    #         unord_list_bool = False
+    #     if not line.startswith(f'{i+1}. '):
+    #         ord_list_bool = False
+    # if quote_bool:
+    #     return BlockType.QUOTE
+    # if unord_list_bool:
+    #     return BlockType.UNORDERED_LIST
+    # if ord_list_bool:
+    #     return BlockType.ORDERED_LIST
 
     return BlockType.PARAGRAPH
